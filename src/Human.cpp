f3e60cf9191bb90aff958c2601e98bc0e5ebf549
Victor Kim <hk1798@nyu.edu>
2017-12-02 04:05:18 -0500

    implemented move, plain move, checkAdjacent fxn
    - move function will either call capturing, cantering, or plain
    - plain move will check possible moves from adjacent positions
    - if position is empty, it will add to valid moves list
    - check adjacent checks if adjcaent positions are valid positions

--- src/Human.cpp
+++ src/Human.cpp
@@ -1,7 +1,7 @@
 #include "Human.h"
 using namespace std;
 
-Human::Human(string& teamColor){
+Human::Human(string& teamColor, Board* theBoard) : board(theBoard){
     //Assign team color (Human should be WHITE)
     color = teamColor;
     
@@ -15,10 +15,54 @@ Human::Human(string& teamColor){
 
 void Human::move(){
     cout << "Human move" << endl;
+    //Check if capturing move is there
+    //if so, human must perform a capturing move
+    //else, human can choose either cantering or plain move
+    plainMove();
+    
 }
 
 vector<Player::Piece>& Human::getPieces(){
     return pieces;
+}
+
+Human::valid_moves Human::plainMove(){
+    valid_moves possibleMoves;
+    for(size_t human_piece = 0; human_piece < pieces.size(); human_piece++){
+        checkAdjacent(&possibleMoves, pieces[human_piece]);
+    }
+    return possibleMoves;
+}
+
+void Human::checkAdjacent(Human::valid_moves* vmList, Player::Piece thePiece){
+    for(size_t rowVal = thePiece.row - 1; rowVal <= thePiece.row + 1; rowVal++){
+        for(size_t colVal = thePiece.column - 1; colVal <= thePiece.column + 1; colVal++){
+            if(rowVal == thePiece.row && colVal == thePiece.column){
+                ;
+            }
+            else{
+                cout << rowVal << ',' << colVal << endl;
+                string posVal = board->getBoard()[rowVal][colVal];
+                if(posVal == "XX"){
+                    cout << "Invalid position. (Border)" << endl;
+                }
+                else if(posVal[0] == 'W'){
+                    cout << "Human piece" << endl;
+                }
+                else if(posVal[0] == 'B'){
+                    cout << "CPU piece" << endl;
+                }
+                else if (posVal == "__"){
+                    cout << "Empty" << endl;
+                    //Add to vmList
+                }
+                else{
+                    cout << "Error" << endl;
+                }
+            }
+        }
+    }
+    cout << endl;
 }
 
 void Human::initialize_pieces(){
--- src/Human.h
+++ src/Human.h
@@ -5,19 +5,33 @@
 #include<string>
 #include<vector>
 #include<map>
+#include<utility>
 
 #include "Player.h"
+#include "Board.h"
+
+class Board;
 
 class Human : protected Player {
 public:
-    Human(std::string& teamColor);
+    Human(std::string& teamColor, Board* theBoard);
     
     void move();
     
+    //Getter for pieces vector
     std::vector<Piece>& getPieces();
+    
+    typedef std::map<int, std::vector<std::pair<int, int>>> valid_moves;
+    
+    valid_moves plainMove();
+    valid_moves canteringMove();
+    valid_moves capturingMove();
+    
+    void checkAdjacent(valid_moves* vmList, Piece thePiece);
     
 private:
     void initialize_pieces();
+    Board* board;
 };
 
 #endif /* Human_h */
