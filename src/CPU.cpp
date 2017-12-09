36e2b166bc979baef2d529e557ab9bccf08db58f
Victor Kim <hk1798@nyu.edu>
2017-12-09 18:55:45 -0500

    Fixed default constructor

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -1,7 +1,7 @@
 #include "CPU.h"
 using namespace std;
 
-CPU::CPU(string& teamColor) : enemy(nullptr), board(nullptr), game(nullptr), alpha(-1000), beta(1000){
+CPU::CPU(string& teamColor) : enemy(nullptr), board(nullptr), game(nullptr), ALPHA_VAL(-1000), BETA_VAL(1000) {
     //Assign team color (CPU should be BLACK)
     color = teamColor;
     
