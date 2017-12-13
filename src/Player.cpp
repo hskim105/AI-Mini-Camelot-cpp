24c67822ead37da4c8cf9de3234a56aa8e4dd7d8
Victor Kim <hk1798@nyu.edu>
2017-12-12 20:48:58 -0500

    Added comments of Player class

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 vector<Player::Piece>& Player::getPieces(){
+    //Return the vector of pieces by reference
     return pieces;
 }
 
 Player::Piece Player::createPiece(int nNumber, string& theTeam, int nRow, int nColumn){
+    //Assign data from parameters to a piece's attributes
     Piece tempPiece;
     tempPiece.number = nNumber;
     tempPiece.team = theTeam;
--- src/Player.h
+++ src/Player.h
@@ -8,6 +8,7 @@
 
 class Player{
 public:
+    //Struct that will represent a piece on the board
     struct Piece{
         int number;
         std::string team;
@@ -16,15 +17,16 @@ public:
     };
     
     virtual void move() = 0;
-    
+
+    //Getter for the vector of pieces
     virtual std::vector<Piece>& getPieces();
     
 protected:
-    std::string color;
-    std::vector<Piece> pieces;
-    std::map<int, std::vector<int> > initial_position;
+    std::string color;          //Team color
+    std::vector<Piece> pieces;  //Vector of pieces (the total number of pieces each player gets)
+    std::map<int, std::vector<int> > initial_position; //Initial starting positions of the pieces
     
-    Piece createPiece(int nNumber, std::string& theTeam, int nRow, int nColumn);
+    Piece createPiece(int nNumber, std::string& theTeam, int nRow, int nColumn); //Create a piece with data from the parameters
 
 };
 
